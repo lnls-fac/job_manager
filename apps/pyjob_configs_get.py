b61c2690e9f30d3568e4d9f97fb2f31fcf4206f1
Fernando <fernandohds564@gmail.com>
2015-09-09 16:30:55 -0300

    number of avalailable lines for printing machine name was increased

--- apps/pyjob_configs_get.py
+++ apps/pyjob_configs_get.py
@@ -62,12 +62,12 @@ def main():
                         lasttime[day] = kl
                     
                     nums += '{:^10d}'.format(vl)
-                print('{:^15s}{:s}'.format('{0:02d}:{1:02d}'
+                print('{:^17s}{:s}'.format('{0:02d}:{1:02d}'
                                            .format(kl[0], kl[1]),nums))  
         return
     
     print('='*78)
-    print('{:15s}{:^7s}{:^7s}{:^10s}{:^10s}{:^10s}{:^11s}{:^6s}'
+    print('{:17s}{:^7s}{:^7s}{:^10s}{:^10s}{:^10s}{:^11s}{:^6s}'
           .format('hostname','State', 'NCPUs','NJPermtd','NJRecvd',
                   'NJRunning', 'Accepting', 'Nice'))
     print('='*78)
@@ -76,7 +76,7 @@ def main():
     for k,v in sorted(ConfigsReceived.items(),key=lambda x: x[0]):
         NumJobs = v.Calendar.get((calendar.day_name[agora.weekday()],
                                   agora.hour, agora.minute), v.defNumJobs)
-        print('{key:15s}{val.active!s:^7s}{val.numcpus:^7d}{N:^10d}'
+        print('{key:17s}{val.active!s:^7s}{val.numcpus:^7d}{N:^10d}'
               '{val.totalJobs:^10d}{val.running:^10d}{acc:^11}'
               '{val.niceness:^6d}'.format(key=k,val=v,N=NumJobs, 
                                           acc='yes' if v.MoreJobs else 'no'))
@@ -85,7 +85,7 @@ def main():
         TJThere += v.totalJobs
         TJRun += v.running
     print('='*78)
-    print('{0:15s}{1:^7s}{2:^7d}{3:^10d}{4:^10d}{5:^10d}{1:^10s}{1:^6s}'
+    print('{0:17s}{1:^7s}{2:^7d}{3:^10d}{4:^10d}{5:^10d}{1:^10s}{1:^6s}'
           .format('Total',' ',TNCPUs, TNAllow, TJThere, TJRun))
         
         
