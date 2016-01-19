74547066327f1b700499303d3d1d135463ceeba6
ximenes <xresende@gmail.com>
2016-01-19 15:37:15 -0200

    Change location of installation file

--- install_services.py
+++ install_services.py
@@ -7,14 +7,14 @@ import socket
 def install_pyjob_run():
     print('installing pyjob_run')
     os.system('stop pyjob_run 2>&1 > /dev/null')
-    os.system('cp -f ./pyjob_run.conf /etc/init/')
+    os.system('cp -f ./src/pyjob_run.conf /etc/init/')
     os.system('start pyjob_run 2>&1 > /dev/null &')
 
 
 def install_pyjob_server():
     print('installing pyjob_server')
     os.system('stop pyjob_server 2>&1 > /dev/null')
-    os.system('cp -f ./pyjob_server.conf /etc/init/')
+    os.system('cp -f ./src/pyjob_server.conf /etc/init/')
     os.system('start pyjob_server 2>&1 > /dev/null &')
 
 
