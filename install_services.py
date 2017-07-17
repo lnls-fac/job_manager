da175060a3ec11c9831eaf9879b438f9017f870a
Fernando <fernandohds564@gmail.com>
2017-07-17 17:51:48 -0300

    Update services to run in Ubuntu16.04.

--- install_services.py
+++ install_services.py
@@ -6,16 +6,28 @@ import socket
 
 def install_pyjob_run():
     print('installing pyjob_run')
+    #ubuntu 14.04
     os.system('stop pyjob_run 2>&1 > /dev/null')
     os.system('cp -f ./src/pyjob_run.conf /etc/init/')
     os.system('start pyjob_run 2>&1 > /dev/null &')
+    #ubuntu 16.04
+    os.system('systemctl stop pyjob_run.service 2>&1 > /dev/null')
+    os.system('cp -f ./src/pyjob_run.service /etc/systemd/system/')
+    os.system('systemctl enable pyjob_run.service 2>&1 > /dev/null &')
+    os.system('systemctl start pyjob_run.service 2>&1 > /dev/null &')
 
 
 def install_pyjob_server():
     print('installing pyjob_server')
+    #ubuntu 14.04
     os.system('stop pyjob_server 2>&1 > /dev/null')
     os.system('cp -f ./src/pyjob_server.conf /etc/init/')
     os.system('start pyjob_server 2>&1 > /dev/null &')
+    #ubuntu 16.04
+    os.system('systemctl stop pyjob_server.service 2>&1 > /dev/null')
+    os.system('cp -f ./src/pyjob_server.service /etc/systemd/system/')
+    os.system('systemctl enable pyjob_server.service 2>&1 > /dev/null &')
+    os.system('systemctl start pyjob_server.service 2>&1 > /dev/null &')
 
 
 install_pyjob_run()
